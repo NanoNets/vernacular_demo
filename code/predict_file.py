719ea7602a6db1be30d3db5044b4daa16b39c2c2
Sarthak Jain <sarthakjain@gmail.com>
2018-07-28 19:26:20 +0530

    no message

--- code/predict_file.py
+++ code/predict_file.py
@@ -1,11 +1,21 @@
 import requests
 import sys
+import json
 
 API_KEY = 'AEajS8_Eomr4xyGMxKDpvubZ3XOf6BAGR_S3lEyAd6H'
+
+def get_top_label(json_response):
+	response = json.loads(json_response)
+	prediction = response["result"][0]["prediction"]
+	max_label_dict = max(prediction, key=lambda x:x['probability'])
+	return max_label_dict["label"], max_label_dict["probability"]
+
 url = 'https://app.nanonets.com/api/v2/ImageCategorization/LabelFile/'
 
 data = {'file': open(sys.argv[2], 'rb'), 'modelId': ('', sys.argv[1])}
 
 response = requests.post(url, auth= requests.auth.HTTPBasicAuth(API_KEY, ''), files=data)
 
-print(response.text)
+label, probability = get_top_label(response.text)
+print "Prediction is:\n", label
+print "\nWith Confidence:\n", probability
--- code/predict_url.py
+++ code/predict_url.py
@@ -1,11 +1,25 @@
 import requests
 import sys
+import json
 
 API_KEY = 'AEajS8_Eomr4xyGMxKDpvubZ3XOf6BAGR_S3lEyAd6H'
+
+def get_top_label(json_response):
+	response = json.loads(json_response)
+	prediction = response["result"][0]["prediction"]
+	max_label_dict = max(prediction, key=lambda x:x['probability'])
+	return max_label_dict["label"], max_label_dict["probability"]
+
 url = 'https://app.nanonets.com/api/v2/ImageCategorization/LabelUrls/'
 
-data = {'urls' : [sys.argv[2]], 'modelId': ('', sys.argv[1])}
+headers = {
+    'accept': 'application/x-www-form-urlencoded'
+}
 
-response = requests.post(url, auth= requests.auth.HTTPBasicAuth(API_KEY, ''), files=data)
+data = {'modelId': sys.argv[1], 'urls' : [sys.argv[2]]}
 
-print(response.text)
+response = requests.request('POST', url, headers=headers, auth=requests.auth.HTTPBasicAuth(API_KEY, ''), data=data)
+
+label, probability = get_top_label(response.text)
+print "Prediction is:\n", label
+print "\nWith Confidence:\n", probability
